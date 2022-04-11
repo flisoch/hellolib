9b6efe6a6d30dd70cde554916eac7110c5454dbe
nyuldashev <nyuldashev@dataarmor.ru>
2022-04-11 16:40:19 +0300

    first commit

---
+++ HelloLib.cpp
@@ -0,0 +1,12 @@
+﻿// HelloTest.cpp : Defines the entry point for the application.
+//
+
+#include "HelloLib.h"
+
+using namespace std;
+
+int hello()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ HelloLib.h
@@ -0,0 +1,8 @@
+﻿// HelloTest.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+int hello();
+// TODO: Reference additional headers your program requires here.
